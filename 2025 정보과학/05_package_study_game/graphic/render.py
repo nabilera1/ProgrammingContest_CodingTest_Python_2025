c0ec567764917a31356e1709d8be15caf3ded390
김동균 <nabilera1@naver.com>
2025-10-29 08:54:41 +0900

    2025 10 29

---
+++ 2025 정보과학/05_package_study_game/graphic/render.py
@@ -0,0 +1,9 @@
+# render.py
+def render_test():
+    print("render")
+
+
+# set PYTHONPATH=C:/doit
+# C:\Users\user\PycharmProjects\ProgrammingContest_CodingTest_Python_2025\2025 정보과학\05_package_study_game>
+
+# set PYTHONPATH=C:\Users\user\PycharmProjects\ProgrammingContest_CodingTest_Python_2025\2025 정보과학\05_package_study_game>
