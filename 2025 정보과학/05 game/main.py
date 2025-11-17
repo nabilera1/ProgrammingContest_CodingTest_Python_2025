31f9d33b4175db885135c6c73a6aa3a4a05eec2e
김동균 <nabilera1@naver.com>
2025-11-17 10:29:05 +0900

    2025 11 17

---
+++ 2025 정보과학/05 game/main.py
@@ -0,0 +1,9 @@
+# import game
+
+import game.sound.echo
+game.sound.echo.echo_test()
+
+print(game.VERSION)
+game.print_version_info()
+# from game.sound import echo
+# print(echo.echo_test())
---
+++ 2025 정보과학/05 game/main1.py
@@ -0,0 +1,4 @@
+from game.sound import *
+import game
+game.render_test()
+echo.echo_test()
